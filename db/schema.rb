03fd9eb25d34dba05c82bfb0996163ffcfdf9682
Henry Baisch <henrybaisch02@gmail.com>
2023-04-06 10:05:35 -0700

    add expense actions and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_162412) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_170231) do
   create_table "expenses", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -21,7 +21,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_162412) do
     t.integer "group_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "recurrence_frequency"
+    t.integer "user_id", null: false
     t.index ["group_id"], name: "index_expenses_on_group_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -45,5 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_162412) do
   end
 
   add_foreign_key "expenses", "groups"
+  add_foreign_key "expenses", "users"
   add_foreign_key "groups", "users"
 end
