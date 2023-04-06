120c15b0da998cfbceb4e8dc8cf8c6c465502010
Henry Baisch <henrybaisch02@gmail.com>
2023-04-06 09:28:37 -0700

    add groups, expenses and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  get 'expenses/index'
+  get 'expenses/show'
+  get 'expenses/new'
+  get 'expenses/edit'
+  get 'groups/index'
+  get 'groups/show'
+  get 'groups/new'
+  get 'groups/edit'
+  devise_for :users
+  root 'groups#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :groups do
+    resources :expenses
+  end
+  
 
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
